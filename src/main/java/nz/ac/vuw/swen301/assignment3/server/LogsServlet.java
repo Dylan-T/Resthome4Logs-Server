b2276b0569a309b19b70af79c06e48655375a1a9
thomsodyla1 <thomsodyla1@ecs.vuw.ac.nz>
2019-06-17 15:18:03 +1200

    doGet done for server

--- src/main/java/nz/ac/vuw/swen301/assignment3/server/LogsServlet.java
+++ src/main/java/nz/ac/vuw/swen301/assignment3/server/LogsServlet.java
@@ -23,34 +23,36 @@ public class LogsServlet extends HttpServlet {
 
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        //Retrieves a log
         response.setContentType("application/json");
+
+        // Check the parameters are given
         if(request.getParameter("limit") == null || request.getParameter("level") == null){
             response.setStatus(400);
         }else {
             List<JsonObject> responseLogs = new ArrayList<JsonObject>();
+
+            // get params
             int limit = Integer.parseInt(request.getParameter("limit"));
             String level = request.getParameter("level");
-            // get logs in order of timestamp
+
+            // Iterate backwards to get them newest to oldest
             int counter = 0;
             for (LogEvent log : logs) {
+                //add only if level is right
                 if (Priority.toPriority(level.toUpperCase()).toInt() >= Priority.toPriority(level.toUpperCase()).toInt()) {
                     responseLogs.add(log.toJson());
                     counter++;
                 }
-                if (counter == limit) break; //If limit is reached stop
+                if (counter == limit) break; //Stop once limit is reached
             }
             PrintWriter out = response.getWriter();
 
+            // Convert logs to json array
             Gson gson = new Gson();
             JsonElement element = gson.toJsonTree(responseLogs, new TypeToken<List<JsonObject>>() {}.getType());
-
-            if (! element.isJsonArray()) {
-
-            }
-
             JsonArray jsonArray = element.getAsJsonArray();
 
+            // write to response
             out.print(jsonArray);
             out.close();
             response.setStatus(200);
@@ -61,6 +63,7 @@ public class LogsServlet extends HttpServlet {
         // Stores a log
         response.setContentType("application/json");
         try {
+            // Read Entity
             HttpEntity entity = new InputStreamEntity(request.getInputStream(), request.getContentLength());
             String jsonLogs = EntityUtils.toString(entity);
 
@@ -68,10 +71,13 @@ public class LogsServlet extends HttpServlet {
                 response.setStatus(400);
                 return;
             }
+
+            // Turn String into JsonArray
             Gson gson = new Gson();
             JsonParser jsonParser = new JsonParser();
             JsonArray arrayFromString = jsonParser.parse(jsonLogs).getAsJsonArray();
 
+            // Add the LogEvents to the logs list
             for(JsonElement json: arrayFromString){
                 logs.add(gson.fromJson(json, LogEvent.class));
             }
